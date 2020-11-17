7e4618fca81b8235d81e94b31705a14172406ddf
Kurt M. Ma. Coll <kurt.ma.coll@gmail.com>
2020-11-17 17:39:16 +0900

    Use void ptr for type flexibility

--- magic_square.c
+++ magic_square.c
@@ -1,17 +1,17 @@
 #include<stdlib.h>
 #include "magic_square.h"
 
-void initialize_int_square (struct int_magic_square *square, int base){
+void initialize_int_square (struct magic_square *square, int base){
     int square_size = base * base;
     square->size = square_size;
     square->base = base;
-    square->cells = malloc(sizeof(struct int_magic_square) + square_size*sizeof(int *));
+    square->cells = (int *)malloc(sizeof(struct magic_square) + square_size*sizeof(int *));
 }
 
-void initialize_char_square (struct char_magic_square *square, int base){
+void initialize_char_square (struct magic_square *square, int base){
     int square_size = base * base;
     square->size = square_size;
     square->base = base;
-    square->cells = malloc(sizeof(struct int_magic_square) + square_size*sizeof(char *));
+    square->cells = (char *)malloc(sizeof(struct magic_square) + square_size*sizeof(char *));
 }
 
--- magic_square.h
+++ magic_square.h
@@ -1,22 +1,15 @@
 #ifndef MAGIC_SQUARE
 #define MAGIC_SQUARE
 
-struct int_magic_square{
+struct magic_square{
     int base;
     int size;
     int index: 1;
-    int* cells;
+    void* cells;
 };
 
-struct char_magic_square{
-    int base;
-    int size;
-    int index: 1;
-    char* cells;
-};
+void initialize_int_square (struct magic_square *square, int base);
 
-void initialize_int_square (struct int_magic_square *square, int base);
-
-void initialize_char_square (struct char_magic_square *square, int base);
+void initialize_char_square (struct magic_square *square, int base);
 
 #endif
