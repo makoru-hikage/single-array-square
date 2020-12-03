2778345d2b94f9977bd0cf60d9f3a8a713db6009
Kurt M. Ma. Coll <kurt.ma.coll@gmail.com>
2020-12-03 19:03:31 +0800

    Use malloc instead. (2)

--- initialisation_tests.c
+++ initialisation_tests.c
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdint.h>
+#include<string.h>
 
 char sator[26] = { ' ',
     's','a','t','o','r',
@@ -28,7 +29,7 @@ void test_int_square(){
     for (int i = 1; i <= isq.size; i++){
         ((int *)isq.cells)[i] = i;
         printf("Index Address: %d \n", (int *)isq.cells + i);
-        printf("Index: %d \n", ((int *)isq.cells)[i]);
+        printf("Index: %d \n", i);
         printf("Content: %d \n", ((int *)isq.cells)[i]);
         printf("-----------\n");
     }
@@ -42,7 +43,8 @@ void test_char_square(){
     struct single_array_square csq;
 
     initialize_square(&csq, base);
-    csq.cells = &sator;
+    csq.cells = (char *) malloc (sizeof(char) * (csq.size + 1));
+    memcpy((char*)csq.cells, sator, csq.size + 1);
 
     int root_index = (uintptr_t)csq.cells;
     printf("Root Address: %d \n", root_index);
@@ -50,12 +52,12 @@ void test_char_square(){
     printf("-----------\n");
 
     for (int i = 1; i <= csq.size; i++){
-        csq.cells++;
-        printf("Index Address: %d \n", csq.cells);
-        printf("Index: %d \n", csq.cells - root_index);
-        printf("Content: %c \n", *(char* )csq.cells);
+        printf("Index Address: %d \n", csq.cells + i);
+        printf("Index: %d \n", i);
+        printf("Content: %c \n", ((char *)csq.cells)[i]);
         printf("-----------\n");
     }
 
+    free(csq.cells);
     printf("==========END\n");
 }
