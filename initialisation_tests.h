f43e4183651596789ac3f8b9941f8a6186b7a726
Kurt M. Ma. Coll <kurt.ma.coll@gmail.com>
2020-12-03 18:43:48 +0800

    Update test file

--- initialisation_tests.h
+++ initialisation_tests.h
@@ -1,7 +1,7 @@
 #ifndef INITIALISATION_TESTS
 #define INITIALISATION_TESTS
 
-#include "single_array_square.h"
+#include "src/single_array_square.h"
 
 void test_int_square();
 void test_char_square();
