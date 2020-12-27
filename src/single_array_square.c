fbf77a0278bcb0ba65eb877787e03af25c565eb7
Kurt M. Ma. Coll <kurt.ma.coll@gmail.com>
2020-12-27 08:31:44 +0800

    Add comments

--- src/single_array_square.c
+++ src/single_array_square.c
@@ -18,11 +18,16 @@ int check_cell_index_validity(struct single_array_square *square, int cell_index
 void print_int_square(struct single_array_square *square){
     int base = square->base;
     int square_size = square->size;
+
+    // Zero padding is required to maintain the visual evenness of the square
+    // when printed to stdout
     int square_size_digits = (square_size == 0) ? 1  : ((int)log10(square_size) + 1);
 
     for (int i = 1; i <= square->size; i++){
         ((int *)square->cells)[i] = i;
         printf ("%0*d ", square_size_digits, ((int *)square->cells)[i]);
+
+        // The rightmost cells are always the multiples of the base.
         if (((int *)square->cells)[i] % square->base == 0){
             printf("\n");
         }
