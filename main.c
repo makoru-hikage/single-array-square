910fcfd32f06ff434716b363d3ac9b1b9cc7e975
Kurt M. Ma. Coll <kurt.ma.coll@gmail.com>
2021-01-05 17:47:24 +0800

    Write print usage

--- main.c
+++ main.c
@@ -6,8 +6,31 @@
 
 typedef int * (*select_line_function) (int, int);
 
-void print_help () {
-    printf("\n USAGE: single_array_square <BASE>\n\n");
+void print_help (char* program_name) {
+
+    printf("USAGE: %s BASE [OPTIONS]...\n\n", program_name);
+    puts(
+        "Print a perfect square with a BASE of a chosen integer.\n"
+        "Selects all the cells when options aren't specified.\n"
+        "\n"
+        "OPTIONS:\n"
+        "  -a                select all cells\n"
+        "  -c INDEX          select all cells within a column by column INDEX\n"
+        "  -d DIRECTION      select a particular slant:\n"
+        "                      5 or 6 for the descending slant\n"
+        "                      4 or 7 for the ascending slant\n"
+        "                      other integers to select all indices of all slants\n"
+        "  -j INDEX          select a particular corner:\n"
+        "                      4 for the top-left corner\n"
+        "                      5 for the top-right corner\n"
+        "                      6 for the down-left corner\n"
+        "                      7 for the down-right corner\n"
+        "                      8 for all corners\n"
+        "                      others select none.\n"
+        "  -m                select the central part of a square\n"
+        "  -r INDEX          select all cells within a row by row INDEX\n"
+    );
+
 }
 
 void print_square(int base, int *selected_cell_indices){
@@ -51,7 +74,7 @@ int main (int argc, char *argv[]) {
         base = atoi(argv[optind]);
         printf("\n");
     } else {
-        print_help();
+        print_help(argv[0]);
         return 0;
     }
 
@@ -108,7 +131,7 @@ int main (int argc, char *argv[]) {
             }
 
             default: {
-                print_help();
+                print_help(argv[0]);
                 return 0;
             }
         }
